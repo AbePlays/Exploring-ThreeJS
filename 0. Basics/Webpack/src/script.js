378db10168e4f6f2f96b31b6c19a1a8b5645f102
Abe <abhi.rawat456@gmail.com>
2021-05-26 10:09:19 +0530

    feat: added webpack

---
+++ 0. Basics/Webpack/src/script.js
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+
+// Create Scene
+const scene = new THREE.Scene();
+
+// Create Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "lime" });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Create object for size
+const size = {
+  width: 800,
+  height: 600,
+};
+
+// Initialize Camera
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+// Position the Camera
+camera.position.z = 3;
+scene.add(camera);
+
+// Initialize Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector(".webgl"),
+});
+renderer.setSize(size.width, size.height);
+
+// Render the scene
+renderer.render(scene, camera);
