912ef06a4058f79a9852528aa638e3c0f3f26f40
Abhishek Rawat <t-abhishek1.rawat@ocltp.com>
2021-05-31 13:05:28 +0530

    feat: project setup

---
+++ 0. Basics/8. 3D Text/src/script.js
@@ -0,0 +1,101 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const canvas = document.querySelector(".webgl");
+
+// Monitoring mouse
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / size.width - 0.5;
+  cursor.y = -(event.clientY / size.height - 0.5);
+});
+
+// Handle browser resizing
+window.addEventListener("resize", () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
+});
+
+// Handle fullscreen
+window.addEventListener("dblclick", () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
+
+// Create Scene
+const scene = new THREE.Scene();
+
+// Create Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "lime" });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Create object for size
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Initialize Camera
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+// Position the Camera
+camera.position.z = 3;
+camera.lookAt(cube.position);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+
+// Add damping
+controls.enableDamping = true;
+
+// Initialize Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  // const elapsedTime = clock.getElapsedTime();
+  // cube.rotation.y = elapsedTime;
+
+  // camera.position.x = cursor.x * 3;
+  // camera.position.y = cursor.y * 3;
+
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 5;
+
+  // camera.lookAt(cube.position);
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
